53300342326db702fd761441f053bb474364b604
Dejan Radovanovic <rdejan93@gmail.com>
2022-06-27 00:06:10 +0200

    Add login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,14 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+const locators = require("../fixtures/locators.json")
+const user = require("../fixtures/user.json")
+
+import { loginPom, LoginPom } from "../e2e/POM/loginPOM"
+
+describe('Login', () => {
+  it('Login with valid user credentials', () => {
+   cy.visit('/');
+   cy.get(locators.login.username).type(user.username)
+   cy.get(locators.login.password).type(user.password)
+   cy.get(locators.login.loginBtn).click()
+   
   })
 })
